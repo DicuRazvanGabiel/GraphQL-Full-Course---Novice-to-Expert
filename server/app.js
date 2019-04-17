17b69fd47d815ce8c2c44c3d851c414d6ceac91c
Dicu Razvan Gabriel <razvan.dicu@itsix.com>
2019-04-17 11:14:18 +0300

    added connection to db

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema')
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0-dt6oi.mongodb.net/test?retryWrites=true', {useNewUrlParser: true}).then(
+    () => console.log('connected to db'),
+    err => console.log(err)
+);
 
 app.use('/graphgl', graphqlHTTP({
     schema,
@@ -10,5 +16,5 @@ app.use('/graphgl', graphqlHTTP({
 }));
 
 app.listen(4000, () => {
-    console.log("noew listening on port: 4000");
+    console.log("now listening on port: 4000");
 });
